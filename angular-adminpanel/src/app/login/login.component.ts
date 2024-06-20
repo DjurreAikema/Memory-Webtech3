5b1d93d782474001360fb9c083594713c968e6b5
Djurre <djurreaikema@gmail.com>
2024-06-20 12:04:57 +0200

    Week 6/7 - Login Styling

--- angular-adminpanel/src/app/login/login.component.ts
+++ angular-adminpanel/src/app/login/login.component.ts
@@ -1,19 +1,48 @@
 import {Component, inject} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Router} from "@angular/router";
+import {MatInputModule} from "@angular/material/input";
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [MatInputModule],
   template: `
-    <form (submit)="login(username.value, password.value, $event)">
-      <input type="text" #username placeholder="Username" required value="Henk">
-      <input type="password" #password placeholder="Password" required value="henk">
-      <button type="submit">Login</button>
+    <form class="login-form" (submit)="login(username.value, password.value, $event)">
+      <mat-form-field appearance="fill">
+        <mat-label>Username</mat-label>
+        <input matInput type="text" #username required value="Henk">
+      </mat-form-field>
+      <mat-form-field appearance="fill">
+        <mat-label>Password</mat-label>
+        <input matInput type="password" #password required value="henk">
+      </mat-form-field>
+      <button type="submit" class="main-button-primary">Login</button>
     </form>
   `,
-  styles: ``
+  styles: [`
+    :host {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      height: 100vh;
+      background-color: rgb(84, 109, 120);
+    }
+
+    .login-form {
+      display: flex;
+      flex-direction: column;
+      width: 300px;
+
+      background-color: white;
+      padding: 50px;
+      border-radius: 5px;
+    }
+
+    mat-form-field {
+      //margin-bottom: 16px;
+    }
+  `]
 })
 export default class LoginComponent {
 
